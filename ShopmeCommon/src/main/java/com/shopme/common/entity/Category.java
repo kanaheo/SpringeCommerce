1780c83e729b62465914e512d49f7f1e6e0eca88
kanaheo <stalvede0421@gmail.com>
2024-09-03 10:22:35 +0900

    category test code end

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -38,6 +38,23 @@ public class Category {
 	@OneToMany(mappedBy = "parent")
 	private Set<Category> children = new HashSet<>();
 	
+	public Category(Integer id) {
+		this.id = id;
+	}
+	
+	public Category() {}
+
+	public Category(String name) {
+		this.name = name;
+		this.alias = name;
+		this.image = "default.png";
+	}
+	
+	public Category(String name, Category parent) {
+		this(name);
+		this.parent = parent;
+	}
+
 	public Integer getId() {
 		return id;
 	}
