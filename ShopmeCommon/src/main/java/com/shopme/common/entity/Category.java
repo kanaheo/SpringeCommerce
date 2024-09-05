cb423ecb745fe5a693e551f57efcf1e6d92a5a44
kanaheo <stalvede0421@gmail.com>
2024-09-05 14:08:37 +0900

    subcategory & categories html list update

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "categories")
@@ -61,6 +62,24 @@ public class Category {
 	public static Category copyIdAndName(Integer id, String name) {
 		Category copyCategory = new Category();
 		copyCategory.setId(id);
+		copyCategory.setName(name);
+		
+		return copyCategory;
+	}
+	
+	public static Category copyFull(Category category) {
+		Category copyCategory = new Category();
+		copyCategory.setId(category.getId());
+		copyCategory.setName(category.getName());
+		copyCategory.setImage(category.getImage());
+		copyCategory.setAlias(category.getAlias());
+		copyCategory.setEnabled(category.isEnabled());
+		
+		return copyCategory;
+	}
+	
+	public static Category copyFull(Category category, String name) {
+		Category copyCategory = Category.copyFull(category);
 		copyCategory.setName(name);
 		
 		return copyCategory;
@@ -127,5 +146,9 @@ public class Category {
 		this.children = children;
 	}
 	
+	@Transient
+	public String getImagePath() {
+		return "/category-images/" + this.id + "/" + this.image;
+	}
 	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -17,7 +17,16 @@ public class MvcConfig implements WebMvcConfigurer {
 		
 		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
 		
-		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + userPhotosPath + "/");
+		registry.addResourceHandler("/" + dirName + "/**")
+			.addResourceLocations("file:/" + userPhotosPath + "/");
+		
+		String categoryImagesDirName = "../category-images";
+		Path categoryImagesDir = Paths.get(categoryImagesDirName);
+		
+		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
+		
+		registry.addResourceHandler("/category-images/**")
+			.addResourceLocations("file:/" + categoryImagesPath + "/");		
 	}
 
 }
